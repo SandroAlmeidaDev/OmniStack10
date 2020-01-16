c5b46b065ee541bb9e4f9c6d7f64236b73dfe7d1
SandroAlmeidaDev <sandroalmeida25@hotmail.com>
2020-01-16 15:06:34 -0300

    Aula 15/10 construindo Interface Web

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from  './services/api'; 
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css'
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
 
 // Os 3 conceitos que montam tudo no React
 // Componente -> Bloco isolado de HTML, CSS e JS o qual não interfere no restante da aplicacao
@@ -6,16 +16,52 @@ import React, { useState } from 'react';
 // Estado -> Informaçoes mantidas pelo componente (Lembrar: imutabilidade)
 
 function App() {
+  // Variaveis no estado do componente
+
+  const [devs, setDevs] = useState([]);
+
+  useEffect(()=> {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const  response = await api.post('/devs', data)
+     //Atualiza tela com novo Dev cadastrado
+      setDevs([...devs, response.data]);
+   }
+
+  /*Funçao contador increment 
   const [counter , setCounter] = useState(0);
   function incrementCounter(){
     setCounter(counter + 1);
   }
-
-  return (
+  */
+  /*return (
     <>
-      <h1>Contador: {counter}</h1>
+     <h1>Contador: {counter}</h1>
       <button onClick={incrementCounter}>Incrementar</button>
     </>
+  );
+  */
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}          
+        </ul>
+      </main>
+    </div>
   );
 }
 
